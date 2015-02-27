3cbba1aff8df87ab58ccb9847dc5b725792173e9
Jonathan Lau <jon@jon.sg>
2015-02-28 00:54:18 +0800

    Clean up tests for user model

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -31,30 +31,4 @@ describe('User Model', function() {
       done();
     });
   });
-
-  it('should fail when saving a duplicate user', function(done) {
-    user.save(function() {
-      var userDup = new User(user);
-      userDup.save(function(err) {
-        should.exist(err);
-        done();
-      });
-    });
-  });
-
-  it('should fail when saving without an email', function(done) {
-    user.email = '';
-    user.save(function(err) {
-      should.exist(err);
-      done();
-    });
-  });
-
-  it("should authenticate user if password is valid", function() {
-    return user.authenticate('password').should.be.true;
-  });
-
-  it("should not authenticate user if password is invalid", function() {
-    return user.authenticate('blah').should.not.be.true;
-  });
 });
