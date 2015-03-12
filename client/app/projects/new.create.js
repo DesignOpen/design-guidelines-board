42367a5ca91a4e5bf06e738502604898367d29b7
Jonathan Lau <jon@jon.sg>
2015-03-12 16:52:43 +0800

    Edit create project page

--- client/app/projects/new.create.js
+++ client/app/projects/new.create.js
@@ -1,31 +1,7 @@
 'use strict';
 
 angular.module('opendesignboardApp')
-  .controller('ProjectNewCreateCtrl', function ($scope, $http, Auth) {
-    var githubLogin = Auth.getCurrentUser().github.login;
-    $scope.githubLogin = githubLogin;
-    $scope.repos = {};
-    $scope.updateAccount = function(account) {
-      var accountType = (account == githubLogin) ? 'user' : 'org';
-      $scope.account = account;
-      $scope.accountType = accountType;
-      if(!$scope.repos[account]) {
-        if(accountType == 'user') {
-          $http.get('/api/github/user/repos').
-            success(function(data, status, headers, config) {
-              $scope.repos[githubLogin] = data;
-            });
-        } else if(accountType == 'org') {
-          $http.get('/api/github/orgs/' +  account + '/repos').
-            success(function(data, status, headers, config) {
-              $scope.repos[account] = data;
-            });
-        }
-      }
-    };
-    $http.get('/api/github/user/orgs').
-      success(function(data, status, headers, config) {
-        $scope.orgs = data;
-      });
-    $scope.updateAccount(githubLogin);
+  .controller('ProjectNewCreateCtrl', function ($scope, $stateParams) {
+    $scope.owner = $stateParams.owner;
+    $scope.repo = $stateParams.repo;
   });
