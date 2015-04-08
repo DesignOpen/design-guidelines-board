6ce72fcefb7cbf8fdd15d6214c54dd45e55de81a
Jonathan Lau <jon@jon.sg>
2015-04-08 18:37:37 +0800

    Add gravatar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,16 +2,15 @@
 
 angular.module('opendesignboardApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth, $state) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
-
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
     $scope.$state = $state;
+    $scope.avatar_url = function(size) {
+      if(!Auth.isLoggedIn()) return null;
+      return Auth.getCurrentUser().github.avatar_url + "&size=" + size;
+    };
 
     $scope.logout = function() {
       Auth.logout();
