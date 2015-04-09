6f80ef46860bb3f989c2560ad35242325af575a6
Jonathan Lau <jon@jon.sg>
2015-04-09 17:44:03 +0800

    Fix bug with async fetching of profile

--- client/app/projects/new.list.controller.js
+++ client/app/projects/new.list.controller.js
@@ -2,30 +2,32 @@
 
 angular.module('opendesignboardApp')
   .controller('ProjectNewListCtrl', function ($scope, $http, Auth) {
-    var githubLogin = Auth.getCurrentUser().github.login;
-    $scope.githubLogin = githubLogin;
-    $scope.repos = {};
-    $scope.updateAccount = function(account) {
-      var accountType = (account == githubLogin) ? 'user' : 'org';
-      $scope.account = account;
-      $scope.accountType = accountType;
-      if(!$scope.repos[account]) {
-        if(accountType == 'user') {
-          $http.get('/api/github/user/repos').
-            success(function(data, status, headers, config) {
-              $scope.repos[githubLogin] = data;
-            });
-        } else if(accountType == 'org') {
-          $http.get('/api/github/orgs/' +  account + '/repos').
-            success(function(data, status, headers, config) {
-              $scope.repos[account] = data;
-            });
+    Auth.isLoggedInAsync(function(isLoggedIn) {
+      var githubLogin = Auth.getCurrentUser().github.login;
+      $scope.githubLogin = githubLogin;
+      $scope.repos = {};
+      $scope.updateAccount = function(account) {
+        var accountType = (account == githubLogin) ? 'user' : 'org';
+        $scope.account = account;
+        $scope.accountType = accountType;
+        if(!$scope.repos[account]) {
+          if(accountType == 'user') {
+            $http.get('/api/github/user/repos').
+              success(function(data, status, headers, config) {
+                $scope.repos[githubLogin] = data;
+              });
+          } else if(accountType == 'org') {
+            $http.get('/api/github/orgs/' +  account + '/repos').
+              success(function(data, status, headers, config) {
+                $scope.repos[account] = data;
+              });
+          }
         }
-      }
-    };
-    $http.get('/api/github/user/orgs').
-      success(function(data, status, headers, config) {
-        $scope.orgs = data;
-      });
-    $scope.updateAccount(githubLogin);
+      };
+      $http.get('/api/github/user/orgs').
+        success(function(data, status, headers, config) {
+          $scope.orgs = data;
+        });
+      $scope.updateAccount(githubLogin);
+    });
   });
