57f94b95c948ae23baca0a50fc51ab4aa2361afb
Jonathan Lau <jon@jon.sg>
2015-04-12 01:01:53 +0800

    Use Promises in favour of callbacks

--- server/api/github/github.controller.js
+++ server/api/github/github.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var User = require('./../user/user.model');
+var Project = require('./../project/project.model');
 var github = require('octonode');
 var common = require('../common.js');
+var Q = require("q");
 
 
 /**
@@ -67,23 +69,30 @@ exports.orgRepos = function(req, res, next) {
  */
 exports.checkRepo = function(req, res, next) {
   var userId = req.user._id;
-  User.findOne({
-    _id: userId
-  }, function(err, user) {
-    if (err) return next(err);
-    if (!user) return res.json(401);
-    if (!user.github.accessToken) return res.json(401);
-    var githubClient = github.client(user.github.accessToken);
-    githubClient.repo(req.params.owner + '/' + req.params.repo).contents('CONTRIBUTING.md', function(err, data, headers) {
-      var response = {hasContributing: false, hasDesignSection: false};
-      if (err) return res.json(response);
-      response.hasContributing = true;
+  var user = Q.nfcall(User.findOne.bind(User), {_id: userId});
+  var accessToken = user.then(function(user) {
+    if (!user || !user.github || !user.github.accessToken) throw new Error('Cannot get access token');
+    return user.github.accessToken;
+  });
+  var githubClient = accessToken.then(function(accessToken) {
+    return github.client(accessToken);
+  });
+  var checkRepo = githubClient.then(function(githubClient) {
+    var repo = githubClient.repo(req.params.owner + '/' + req.params.repo);
+    return Q.nfcall(repo.contents.bind(repo), 'CONTRIBUTING.md').spread(function(data, headers) {
       var buffer = new Buffer(data.content, data.encoding);
       var data = buffer.toString();
-      if(common.getDesignSection(data)) {
-        response.hasDesignSection = true;
-      }
-      res.json(response);
+      return [true, common.getDesignSection(data) != null];
+    }, function() {
+      return [false, false];
     });
   });
+  checkRepo.spread(function(hasContributing, hasDesignSection) {
+    return res.json({
+      hasContributing: hasContributing,
+      hasDesignSection: hasDesignSection
+    });
+  }, function(err) {
+    next(err);
+  }).done();
 };
